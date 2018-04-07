2d3abf0996c647fddd0755ae36b5153cddb5b5b6
janoshrubos <janoshrubos@icloud.com>
2018-04-08 00:43:01 +0200

    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"# revils" 
+# Betting Bot for Sliver.tv
+## Installation
+
+This script was tested on **Firefox** and **Chrome**.
+
+### Steps
+1. Install TamperMonkey extension. [Firefox](https://addons.mozilla.org/hu/firefox/addon/tampermonkey/) [Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=hu)
+2. Click on the icon in your browser and select **Create a new script...**
+3. Copy the code from **rev.js**.
+4. In the end of the script you will find a bet() function. If you would like the bet on only yes answer ("A" or "Y"), write **bet("A")** there.
+5. Restart your browser.
+6. Done.
--- rev.js
+++ rev.js
@@ -1,156 +1,221 @@
-function $(className) {
-  return document.getElementsByClassName(className);
-}
+// ==UserScript==
+// @name         Sliver.tv Bot (A)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Sliver.tv betting bot
+// @author       Janos Hrubos
+// @include      https://www.sliver.tv/win/fortnite
+// @include      https://www.sliver.tv/win/pubg
+// @include      https://www.sliver.tv/win/csgo
+// @match        https://www.sliver.tv/win/fortnite
+// @match        https://www.sliver.tv/win/pubg
+// @match        https://www.sliver.tv/win/cs
+// @grant        none
+// ==/UserScript==
 
-function getBetTitle(betCard)
-{
-  return getCards()[betCard].firstChild.innerHTML;
-}
+(function() {
+    var contentPanel = getEBCN('content-panel')[0];
+    var miniGamePanel = getEBCN('mini-game-panel')[0];
 
-function getAnswer(betCard, ans)
-{ // ans = 0 or 1
-  return getCards()[betCard].children[1].children[0].children[ans];
-}
+    var rafflePrizes = getEBCN('prize-container');
+    var fixedWallet = getEBCN('fixed-wallet')[0];
 
-function whatIsSelected(betCard)
-{
-  var selected = "none";
-  var sel = false;
-  for(let i = 0; i < getCards()[betCard].children[1].children[0].children.length; i++)
-  {
-    if(getCards()[betCard].children[1].children[0].children[i].classList.contains("is-selected"))
+    var betCards = getEBCN('bet-card');
+    var open = getEBCN('open-button');
+    var openCrate = getEBCN('open-crate-button');
+    var cont = getEBCN('continue-button');
+
+    function getEBCN(className)
     {
-      selected = i;
-      sel = true;
+        return document.getElementsByClassName(className);
     }
-  }
-  if (sel)
-  {
-    return parseInt(selected)
-  }
-  else
-  {
-    return selected;
-  }
-}
 
-function getCards()
-{
-  return $('bet-card');
-}
-
-function isPlaced(betCard)
-{
-  return getCards()[betCard].children[1].children[0].children[0].classList.contains("is-static");
-}
-
-function print()
-{
-  for (let i = 0; i < getCards().length; i++)
-  {
-    console.log("--------- " + getBetTitle(i));
-    if (whatIsSelected(i) == "none")
+    function getBetTitle(betCard)
     {
-      console.log("--- No bet selected")
+        return getCards()[betCard].firstChild.innerHTML;
     }
-    else
+
+    function getAnswer(betCard, ans)
+    { // ans = 0 or 1
+        return getCards()[betCard].children[1].children[0].children[ans];
+    }
+
+    function whatIsSelected(betCard)
     {
-      console.log("-- Your answer: " + getCards()[i].children[1].children[0].children[whatIsSelected(i)].children[1].innerText);
+        var selected = "none";
+        var sel = false;
+        for(let i = 0; i < getCards()[betCard].children[1].children[0].children.length; i++)
+        {
+            if(getCards()[betCard].children[1].children[0].children[i].classList.contains("is-selected"))
+            {
+                selected = i;
+                sel = true;
+            }
+        }
+        if (sel)
+        {
+            return parseInt(selected);
+        }
+        else
+        {
+            return selected;
+        }
     }
-    console.log('\n');
-  }
-}
 
-function bet(opt)
-{
-  var option = -1;
-  switch (opt)
-  {
-    case "A":
-      option = 0;
-      break;
-    case "B":
-      option = 1;
-      break;
-    case "C":
-      option = 2;
-      break;
-    case "D":
-      option = 3;
-      break;
-    case "E":
-      option = 4;
-      break;
-    default:
-  }
-
-  for (let i = 0; i < getCards().length; i++)
-  {
-    if (!isPlaced(i))
+    function getCards()
     {
-      getCards()[i].children[1].children[0].children[option].click();
-      console.log("##### ALL BETS PLACED #####");
+        return getEBCN('bet-card');
     }
-  }
-}
 
-function openCrate()
-{
-  var open = $('open-button');
-  var openCrate = $('open-crate-button');
-  var cont = $('continue-button');
-
-  if (open.length != 0)
+    function isPlaced(betCard)
     {
-      open[0].click();
-      console.log("Opened crate.")
+        return getCards()[betCard].children[1].children[0].children[0].classList.contains("is-static");
     }
-    if (openCrate.length != 0 && !openCrate[0].classList.contains("is-disabled"))
+
+    function print()
     {
-      openCrate[0].click();
-      console.log("Rolled crate.")
+        for (let i = 0; i < getCards().length; i++)
+        {
+            console.log("--------- " + getBetTitle(i));
+            if (whatIsSelected(i) == "none")
+            {
+                console.log("--- No bet selected");
+            }
+            else
+            {
+                console.log("-- Your answer: " + getCards()[i].children[1].children[0].children[whatIsSelected(i)].children[1].innerText);
+            }
+            console.log('\n');
+        }
     }
-    if (cont.length != 0)
+
+    function bet(opt)
     {
-      cont[0].click();
-      console.log("Continue after crate.")
+        var option = -1;
+        switch (opt)
+        {
+            case "A":
+                option = 0;
+                break;
+            case "B":
+                option = 1;
+                break;
+            case "C":
+                option = 2;
+                break;
+            case "D":
+                option = 3;
+                break;
+            case "E":
+                option = 4;
+                break;
+            default:
+        }
+
+        for (let i = 0; i < getCards().length; i++)
+        {
+            if (!isPlaced(i))
+            {
+                getCards()[i].children[1].children[0].children[option].click();
+                console.log("##### ALL BETS PLACED #####");
+            }
+        }
     }
-}
 
-function style()
-{
-  contentPanel = $('content-panel')[0];
-  miniGamePanel = $('mini-game-panel')[0];
+    function openCrates()
+    {
+        if (open.length != 0)
+        {
+            open[0].click();
+            console.log("Opened crate.");
+        }
+        if (openCrate.length != 0 && !openCrate[0].classList.contains("is-disabled"))
+        {
+            openCrate[0].click();
+            console.log("Rolled crate.");
+        }
+        if (cont.length != 0)
+        {
+            cont[0].click();
+            console.log("Continue after crate.");
+        }
+    }
 
-  rafflePrizes = $('prize-container');
-  fixedWallet = $('fixed-wallet')[0];
+    function style()
+    {
 
-  betCards = $('bet-card');
+        contentPanel.innerHTML = "";
+        fixedWallet.innerHTML = "";
+        miniGamePanel.style.flexBasis = "100%";
 
-  contentPanel.innerHTML = "";
-  fixedWallet.innerHTML = "";
-  miniGamePanel.style.flexBasis = "100%";
+        //bet cards
+        for (let i = 0; i < betCards.length; i++)
+        {
+            centerize(betCards[i]);
+        }
 
-  //bet cards
-  for (let i = 0; i < betCards.length; i++)
-  {
-    centerize(betCards[i]);
-  }
+        for (let i = 0; i < rafflePrizes.length; i++)
+        {
+            centerize(rafflePrizes[i]);
+        }
+    }
 
-  for (let i = 0; i < rafflePrizes.length; i++)
-  {
-    centerize(rafflePrizes[i]);
-  }
-}
+    function centerize(array)
+    {
+        array.classList.add("center");
+    }
 
-function centerize(array)
-{
-  array.classList.add("center");
-}
+    // diamonds-inventory-type
+    function openCratesFromInv()
+    {
+        var crateInterval = setInterval(function()
+                                        {
+            if (getEBCN('continue-button').length == 0  && getEBCN('crate-inventory-type').length > 0)
+            {
+                for (let i = 0 ; i < getEBCN('crate-inventory-type').length; i++)
+                {
+                    getEBCN('crate-inventory-type')[0].children[3].click();
+                    setTimeout(function() {getEBCN('open-crate-button')[0].click();}, 1000);
+                    setTimeout(function() {getEBCN('continue-button')[0].click();}, 13000);
+                }
+            }
+            else
+            {
+                clearInterval(crateInterval);
+            }
+        }, 16000);
+    }
 
-setInterval(function()
-{
-    style()
-    bet("A");
-    openCrate();
-}, 1000);
+    function claimAllDiamonds()
+    {
+        var length = getEBCN('diamonds-inventory-type').length;
+        for(let i = 0; i < length; i++)
+        {
+            setTimeout(function()
+                       {
+                getEBCN('diamonds-inventory-type')[i].children[3].children[0].click();
+            }, 2000);
+        }
+        console.log(length + " diamond(s) opened.");
+    }
+
+    function claimAllCoins()
+    {
+        var length = getEBCN('coins-inventory-type').length;
+        for(let i = 0; i < length; i++)
+        {
+            setTimeout(function()
+                       {
+                getEBCN('coins-inventory-type')[i].children[3].children[0].click();
+            }, 1000);
+        }
+        console.log(length + " coin(s) opened.");
+    }
+
+
+    setInterval(function()
+                {
+        bet("A");
+    }, 1000);
+
+})();
